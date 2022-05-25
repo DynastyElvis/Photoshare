4966e9325f9a05e3c1484964adbb2b0440bb153e
DynastyElvis <kipkemoi.rono@student.moringaschool.com>
2022-05-25 21:47:16 +0300

    ft: Category and photo class inherits from the Model class

--- photoshare/photos/models.py
+++ photoshare/photos/models.py
@@ -1,3 +1,24 @@
+from email.mime import image
 from django.db import models
 
 # Create your models here.
+class Category(models.Model): # The Category class inherits from the Model class
+    name = models.CharField(max_length=200, null=False, blank=False)
+    def __str__(self):
+        return self.name
+    
+        """
+        The Category class inherits from the Model class
+        """
+
+class Photo(models.Model): # inherits from the Model class
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL)
+    image = models.ImageField(null=False, blank=False)
+    description = models.CharField(max_length=200, null=False, blank=False)
+    def __str__(self):
+        return self.description
+    
+        """
+        The photo class inherits from the Model class
+        """
+        

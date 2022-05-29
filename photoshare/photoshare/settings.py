0b59bd0245099e6fac83ee8af5aba402386a0857
DynastyElvis <kipkemoi.rono@student.moringaschool.com>
2022-05-29 19:54:15 +0300

    ft: added screenshots

--- photoshare/photoshare/settings.py
+++ photoshare/photoshare/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+import os
+#import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -127,7 +129,11 @@ STATICFILES_DIRS = [
 ]
 MEDIA_ROOT = BASE_DIR / 'static/images'
 STATIC_ROOT = BASE_DIR / 'static/static' 
-    
+
+
+#STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+#django_heroku.settings(locals())
+ 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 

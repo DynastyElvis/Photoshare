df6fd49f8d090970e405772c518f5a695c48764e
DynastyElvis <kipkemoi.rono@student.moringaschool.com>
2022-06-08 11:55:18 +0300

    ft: heroku deplyment

--- photoshare/photoshare/settings.py
+++ photoshare/photoshare/settings.py
@@ -59,12 +59,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-=oh4vu-j!pm_i=7%9!-))siss8rbgkc(sr^+zwj&sumau2j8b7'
+SECRET_KEY = '0000'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['elvis-gallery.herokuapp.com/', '127.0.0.1']
 
 
 # Application definition
@@ -77,6 +77,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'photos.apps.PhotosConfig',
+    'photoshare'
 ]
 
 MIDDLEWARE = [
@@ -156,8 +157,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
-MEDIA_URL = 'images/'
+STATIC_URL = '/static/'
+MEDIA_URL = '/images/'
 
 
 
